b850f746b42b3d9abdea94231099b4b2b60eab3b
alikeli <eli-com2@yandex.ru>
2023-03-06 19:46:42 +0100

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,19 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int reverse = 0;
+        while (number != 0) {
+            reverse = reverse * 10;
+            reverse = reverse + number % 10;
+            number = number / 10;
+        }
+        System.out.println(reverse);
+
+
+    }
+
+    public static void main(String[] args) {
+        NumberReverter x = new NumberReverter();
+        x.revert(456);
     }
 }
